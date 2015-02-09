17522ad5cda291cb00d5b71b3498ed8604c566da
voper45 <voper45@gmail.com>
2015-02-08 23:32:50 -0500

    not much

---
+++ Assets/Structures/Barrier.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrier : MonoBehaviour {
+	protected Team team;
+	
+	// Use this for initialization
+	void Start () {
+		team = transform.root.GetComponentInChildren< Team >();
+		SetTeamColor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void SetTeamColor() {
+		bool temp;
+		TeamColor[] teamColors = GetComponentsInChildren< TeamColor >();
+		foreach (TeamColor teamColor in teamColors) {
+			temp = false;
+			foreach (Material matt in teamColor.renderer.materials) {
+				
+				print ("Mat Name: " + matt.name);
+				if (matt.name == "Glow (Instance)") {
+					matt.SetColor  ("_TintColor", team.glowColor );
+					temp = true;
+				}
+				if (matt.name == "Team (Instance)") {
+					matt.color = team.teamColor;
+					temp = true;
+				}
+				
+				
+			}
+		}
+	}
+}
